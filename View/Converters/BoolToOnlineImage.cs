0c41905701248270fdbf0a1d5e6a067f56b84911
Jonas De Ridder <jonasderidder64@gmail.com>
2021-03-16 17:33:08 +0100

    Fix design view bug

--- View/Converters/BoolToOnlineImage.cs
+++ View/Converters/BoolToOnlineImage.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media.Imaging;
 
@@ -15,8 +17,14 @@ namespace Project_DeRidderJonas_HypixelApi.View.Converters
         {
             bool isOnline = (bool)value;
 
-            if (isOnline) return new BitmapImage(new Uri("pack://application:,,,/Resources/Images/Online.png", UriKind.Absolute));
-            else return new BitmapImage(new Uri("pack://application:,,,/Resources/Images/Offline.png", UriKind.Absolute));
+            if (!DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                if (isOnline) return new BitmapImage(new Uri("pack://application:,,,/Resources/Images/Online.png", UriKind.Absolute));
+                else return new BitmapImage(new Uri("pack://application:,,,/Resources/Images/Offline.png", UriKind.Absolute));
+            }
+
+            return new BitmapImage();
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
